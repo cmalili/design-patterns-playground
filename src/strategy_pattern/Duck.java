d3a47fbc11b6fc9e82399d886d20111ad5fbaff4
Christopher, Malili <malilichristopher@gmail.com>
2025-08-01 17:26:21 -0700

    Created Duck class

--- src/strategy_pattern/Duck.java
+++ src/strategy_pattern/Duck.java
@@ -1,5 +1,23 @@
 package strategy_pattern;
 
-public class Duck {
-
+public abstract class Duck {
+	
+	FlyBehaviour flyBehaviour;
+	QuackBehaviour quackBehaviour;
+	public Duck() {
+	}
+	
+	public abstract void display();
+	
+	public void performFlyBehaviour() {
+		flyBehaviour.fly();
+	}
+	
+	public void performQuackBehaviour() {
+		quackBehaviour.quack();
+	}
+	
+	public void swim() {
+		System.out.println("All ducks float, even decoys!");
+	}
 }
