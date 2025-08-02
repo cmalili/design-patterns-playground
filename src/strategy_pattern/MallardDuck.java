2cadd744adbec4c6f817df9269227df838d4a905
Christopher, Malili <malilichristopher@gmail.com>
2025-08-01 19:44:32 -0700

    Created MallardDuck class and MiniDuckSimulation classes

---
+++ src/strategy_pattern/MallardDuck.java
@@ -0,0 +1,15 @@
+package strategy_pattern;
+
+public class MallardDuck extends Duck {
+	
+	public MallardDuck() {
+		quackBehaviour = new Quack();
+		flyBehaviour = new FlyWithWings();
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("I am a real mallard duck.");
+	}
+	
+}
---
+++ src/strategy_pattern/MiniDuckSimulator.java
@@ -0,0 +1,10 @@
+package strategy_pattern;
+
+public class MiniDuckSimulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
